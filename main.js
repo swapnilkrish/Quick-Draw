fd63aa919f52994ae8d4ab4d695d18b5583199d2
swapnilkrish <86659449+swapnilkrish@users.noreply.github.com>
2022-05-27 12:39:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -37,10 +37,33 @@ function draw() {
     if (drawn_sketch == sketch) {
         answer_holder = "set";
         score++;
-        document.getElementById('score').innerHTML = 'Score; ' + score;
+        document.getElementById('score').innerHTML = 'Score;' + score;
     }
 
 }
 function classifyCanvas() {
     classifier.classify(canvas, gotResult);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    drawn_sketch = results[0].label;
+    document.getElementById('label').innerHTML = 'your Sketch:' + drawn_sketch;
+    document.getElementById('confidence').innerHTML = 'confidence' + Math.round(results[0].confidence * 100) + '%';
+}
+function check_sketch() {
+    timer_counter++;
+    document.getElementById('time').innerHTML = 'Timer:' + timer_counter;
+    console.log(timer_counter);
+    if (timer_counter > 400) {
+        timer_counter = 0;
+        timer_check = "completed";
+    }
+    if (timer_check == "completed" || answer_holder == "set");
+    timer_check = "";
+    answer_holder = "";
+    updateCanvas();
 }
